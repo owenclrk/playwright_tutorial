39598650fbf4d1b34184e8446c186c8d081fa981
owenclrk <owenclrk@gmail.com>
2026-06-25 18:49:36 +0200

    Add test for login failure with invalid credentials

--- test_login.py
+++ test_login.py
@@ -16,3 +16,13 @@ def test_login_page_loads(page: Page):
     success_message = page.locator("#flash")
     expect(success_message).to_be_visible()
     expect(success_message).to_contain_text("You logged into a secure area")
+
+def test_fails_with_invalid_creditentials(page: Page):
+    page.goto("https://the-internet.herokuapp.com/login")
+    page.locator('#username').fill("wrong_username")
+    page.locator('#password').fill("wrong_password")
+    page.locator("button[type='submit']").click()
+
+    error_message = page.locator("#flash")
+    expect(error_message).to_be_visible()
+    expect(error_message).to_contain_text("Your username is invalid!")
