b76bd6118dea9fd7607d06c5c2b8c38aae73d193
owenclrk <owenclrk@gmail.com>
2026-06-25 18:34:23 +0200

    Implement login page test functionality

--- test_login.py
+++ test_login.py
@@ -1,4 +1,14 @@
 from playwright.sync_api import Page, expect
 
+
 def test_login_page_loads(page: Page):
-    pass
+    page.goto("https://the-internet.herokuapp.com/login")
+    expect(page).to_have_title("The Internet")
+
+    username_field = page.locator("#username")
+    username_field.fill("tomsmith")
+
+    password_field = page.locator("#password")
+    password_field.fill("SuperSecretPassword!")
+
+    page.locator("button[type='submit']").click()
